553f2287a27b0801a15703f532d432ab662a829d
mosch19 <sportfreak206@gmail.com>
2017-04-29 13:12:25 -0400

    updated

--- restoration.cpp
+++ restoration.cpp
@@ -10,16 +10,41 @@ using namespace std;
 
 int main()
 {
+    CImg<float> original;
     CImg<float> image;
     CImg<float> image2;
-    string location = "/home/mosch/Documents/Restoration/lena_full.jpg";
-    image.load(location.c_str());
-    image2 = image.get_RGBtoYCbCr().get_channel(0);
-    CImgDisplay main_disp(image, "Lena"), draw_disp(image2, "Gray Lena");
 
-    while(!main_disp.is_closed)
+    float MSE = 0;
+
+    //int height, width = 0;
+    //height = original.height;
+    //width = original.width;
+
+    //CImg<int> test(int width, int height);
+
+    string location = "/home/mosch/Documents/Restoration/lena_full.jpg";
+    original.load(location.c_str());
+    image = original.get_RGBtoYCbCr().get_channel(0);
+    image2 = image;
+
+    cimg_forXY(image, x, y)
     {
-        main_disp.wait();
+        if (image(x,y) < 100)
+        {
+            image(x,y) = 0;
+            cout << "value: " << image(x,y) << endl;
+        }
+    }
+    
+    //mess it up
+    image2.noise(40);
+    image2.blur(2.5);
+    CImgDisplay org_disp(original, "Original"), main_disp(image, "Greyscale Lena"), draw_disp(image2, "Blur/Noise Lena");
+
+    //display the images
+    while(!draw_disp.is_closed)
+    {
+        draw_disp.wait();
     }
 
 
