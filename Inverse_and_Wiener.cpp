d5225e53427f55dcf56e8411072337d05939c411
Matthew Tomaro <mtomaro95@gmail.com>
2017-05-01 14:57:11 -0400

    Update and rename Inverse_Filter.cpp to Inverse_and_Wiener.cpp

--- Inverse_and_Wiener.cpp
+++ Inverse_and_Wiener.cpp
@@ -27,9 +27,88 @@ using namespace std;
 
 
 
+Mat padd_image(Mat I){
+	Mat padded;
+	int m = getOptimalDFTSize( I.rows );
+	int n = getOptimalDFTSize( I.cols ); // on the border add zero pixels
+	copyMakeBorder(I, padded, 0, m - I.rows, 0, n - I.cols, BORDER_CONSTANT, Scalar::all(0));
+	return padded;
+}
+
+Mat get_dft(Mat I){
+	Mat image;
+	I.convertTo(image, CV_32F);
+	Mat planes[] = {Mat_<float>(image), Mat::zeros(image.size(), CV_32F)};
+	Mat complexI;
+	merge(planes, 2, complexI);
+	dft(complexI, complexI);
+	return complexI;
+}
+
+Mat get_spectrum(Mat I){
+	Mat complexI = get_dft(I);
+	Mat planes[2];
+	split(complexI, planes);                   // planes[0] = Re(DFT(I), planes[1] = Im(DFT(I))
+	magnitude(planes[0], planes[1], planes[0]);// planes[0] = magnitude
+	Mat magI = planes[0];
+	multiply(magI,magI,magI);
+	return magI;
+}
+
+
+
+Mat rand_noise(Mat I, int stddev){
+	Mat noise = Mat::zeros(I.rows, I.cols, CV_32F);
+	randn(noise,Scalar::all(0), Scalar::all(stddev));
+	return noise;
+}
+
+Mat with_noise(Mat image, int stddev){
+	Mat noise(image.rows, image.cols, CV_8U);
+	rand_noise(image, stddev).convertTo(noise, CV_8U);
+	Mat noisy = image.clone();
+	noisy += noise;
+	return noisy;
+}
+
+
+Mat wiener2(Mat I, Mat image_spectrum, int noise_stddev){
+	Mat padded = padd_image(I);
+	Mat noise = rand_noise(padded, noise_stddev);
+	Mat noise_spectrum = get_spectrum(noise);
+
+	Scalar padded_mean = mean(padded);
+
+	Mat planes[2];
+	Mat complexI = get_dft(padded);
+	split(complexI, planes);	// planes[0] = Re(DFT(I), planes[1] = Im(DFT(I))
+
+	Mat factor = image_spectrum / (image_spectrum + noise_spectrum);
+	multiply(planes[0],factor,planes[0]);
+	multiply(planes[1],factor,planes[1]);
+
+
+	merge(planes, 2, complexI);
+	idft(complexI, complexI);
+	split(complexI, planes);
+//	normalize(planes[0], planes[0], 0, 128, CV_MINMAX );
+	Scalar enhanced_mean = mean(planes[0]);
+	double norm_factor =  padded_mean.val[0] / enhanced_mean.val[0];
+	multiply(planes[0],norm_factor, planes[0]);
+	Mat normalized;
+	planes[0].convertTo(normalized, CV_8UC1);
+	return normalized;
+}
+
+
+
 int main(){
 
-	Mat image = imread("lena_full.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+	int noise_stddev;
+	cout << "Choose noise standard dev" << endl;
+	cin >> noise_stddev;
+
+	Mat image = imread("lena.jpg", CV_LOAD_IMAGE_GRAYSCALE);
 	imshow("Image", image);
 	//Mat blurredImage1;
 	Mat blurredImage;
@@ -108,15 +187,37 @@ int main(){
 	
 	Mat blurkernal;
 	divide(complexI2, complexI, blurkernal);
-	//imshow ("DFT of oringal / blurred", blurkernal);
+	Mat visualBlurKernal;
+	divide(magI2, magI, visualBlurKernal);
+	imshow ("DFT of oringal / blurred", visualBlurKernal);
 	Mat Xf;
 	divide(complexI2,blurkernal, Xf);
-	//imshow ("Xf", Xf);
+	Mat visualXf;
+	divide (magI2, visualBlurKernal, visualXf);
+	imshow ("Xf", visualXf);
 
 	Mat inverseDFT;
 	idft(Xf, inverseDFT, DFT_INVERSE | DFT_REAL_OUTPUT);
 	normalize(inverseDFT, inverseDFT, 0, 1, CV_MINMAX);
+	//cout << inverseDFT << endl;
 	imshow("Reconstructed", inverseDFT);
+
+	Mat padded3=padd_image(image);
+	Mat noisy;
+	noisy = with_noise(padded, noise_stddev);
+
+	//GaussianBlur(noisy, noisy, Size (3,3), 100, 0, 4);
+
+	Mat sample(padded3.rows, padded3.cols, CV_8U);
+	resize(image, sample, sample.size());
+	Mat spectrum = get_spectrum(sample);
+	Mat enhanced = wiener2(noisy, spectrum, noise_stddev);
+	
+
+	imshow("Noisy image before Wiener", noisy);
+	imshow("Enhanced Image after Wiener", enhanced);
+
+
 
 	waitKey(0);
 	destroyAllWindows();
